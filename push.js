f7d0916768b21cc11029c58c1b52385ec0178ba3
Amit Kumar <amit.r.kumar@hotmail.com>
2020-03-31 23:20:46 -0400

    fix

--- push.js
+++ push.js
@@ -4,9 +4,21 @@ var push = require('web-push');
 let vapidKeys = {
   publicKey: 'BBHSUmckwsWjPz44NS90YQCp1Kx2gD9V8KAlxt3Jusf1OcFhpVqr_wRK061f8q2DAHN_mNDNSYXL-s2KKAdIk4E',
   privateKey: 'MTpZfk3hZuEel004xq0RXCfypKO7qtX-WJLCb_f8p3Y'
-}
+};
 
-push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails(
+	'mailto:test@code.com', 
+	vapidKeys.publicKey, 
+	vapidKeys.privateKey
+);
 
-let sub = {};
+let sub = {
+	endpoint: 'https://fcm.googleapis.com/fcm/send/ctu6XYlJ3Jk:APA91bGL3HPINPRMDQflDXnLl0fuiVaVkxiQC43rxjxAEXkHMGm-UOu7mpxHL8B9rHshnJHdpVYVr1zO-bdmC3kiIKsOzzYlCZu6lJfM4UPQvgHj2nUDnirRhfy2VixzPGThZ7CrAPFG',
+	expirationTime: null,
+	keys: {
+		p256dh: 'BBsHXG6hn_rgPOMxqdXQIBZ90gUD12Wpy4R1hSeDcw-G7y7qLp5CBkzB0QBaGuxm1x9V6N0_sZ37osnUoBLUtKM',
+		auth: 'CKU3aGdMxXYvAn3S4NFwWQ'
+	}
+};
+
 push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('push', () => {
-	self.registration.sendNotification('test message', {});
+self.addEventListener('push', function(e) {
+	var options = {
+		body: 'Notification generated from a push',
+		icon: 'images/example.png',
+		vibrate: [100, 50, 100],
+		data: {
+			dateOfArrival: Date.now(),
+			primaryKey: '2'
+		},
+		actions: [
+			{
+				action: 'explore',
+				title: 'Explore this new wrld',
+				icon: 'images/checkmark.png'
+			},
+			{
+				action: 'closing',
+				title: 'Close',
+				icon: 'images/xmark.png'
+			}
+		]
+	};
+	
+	e.waitUntil(
+		self.registration.showNotification('Hello World', options)
+	);
 });
